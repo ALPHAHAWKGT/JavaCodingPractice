dc70425aa68b84282eff722c4939a04b199ee8ca
ALPHAHAWKGT <90155032+ALPHAHAWKGT@users.noreply.github.com>
2021-09-11 23:00:31 -0700

    Update ForLoop.java

--- ForLoop.java
+++ ForLoop.java
@@ -3,10 +3,13 @@ public class ForLoop {
         ForLoop forLoop = new ForLoop();
 //        forLoop.printXTimes(10, "Dhruv Is Cool");
         forLoop.printXTimes(0, "Dhruv Is Cool");
-        System.out.println(forLoop.powers(2,-2));
+        System.out.println(forLoop.powers(100,2));
+        System.out.println(forLoop.areaOfSquare(10));
+        System.out.println(forLoop.areaOfRectangle(5,2));
+        System.out.println(forLoop.areaOfCircle(0));
     }
     public void printXTimes(int x, String message){
-        for(int i = 0; i < x; i++){
+        for(int i = 1; i < x; i++){
             System.out.println(message);
         }
     }
@@ -20,6 +23,41 @@ public class ForLoop {
             result = result * base;
         }
         return result;
-
     }
-}
+    /* areaOfSquare is a function that takes the length of the side of a square as an input and squares the length to
+    return the area of the square. It also throws an exception when the length of the side is negative, which is not
+    possible */
+    public double areaOfSquare (double side) {
+        double area = 0;
+        if (side < 0){
+            throw new IllegalArgumentException("Error, side of a square can never be negative");
+        }
+        area = side * side;
+        return area;
+    }
+    /* areaOfRectangle is a function that takes the length and width of a rectangle as an input and multiplies both to
+    return the area of the rectangle. It also throws an exception when the length, width, or both are negative, which
+    is not possible */
+    public double areaOfRectangle (double length, double width) {
+        double area = 0;
+        if (length < 0) {
+            throw new IllegalArgumentException("Error, length of a rectangle can never be negative");
+        }
+        if (width < 0) {
+            throw new IllegalArgumentException("Error, width of a rectangle can never be negative");
+        }
+            area = length * width;
+        return area;
+    }
+    /* areaOfCircle is a function that takes the radius of a circle as an input and multiplies it by 44/7 to return the
+    area of the rectangle. It also throws an exception when the radius is negative, which is not possible
+     */
+    public double areaOfCircle (double radius) {
+        double area = 0;
+        if (radius < 0) {
+            throw new IllegalArgumentException("Error, radius of a circle can never be negative");
+        }
+        area = radius * radius * 22/7;
+        return area;
+    }
+    }
